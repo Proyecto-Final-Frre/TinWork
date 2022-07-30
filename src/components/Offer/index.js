e5249e7edb77c1616b9c6fcb662890ce69a4a649
Matias Saravia <matiasaravia52@gmail.com>
2022-07-30 12:43:26 -0300

    se agrega libreria de iconos

--- src/components/Offer/index.js
+++ src/components/Offer/index.js
@@ -1,19 +1,22 @@
-import { Button, Card, Form } from "react-bootstrap";
+import React from "react";
 import "./style.css";
-import busc from "../../logos/buscar.PNG";
-
-
+import { AiOutlineFileSearch } from "react-icons/ai";
 const Offer = () => {
   return (
-    
     <div className="card-offer">
-    <div class="card text-center "  >
-    <div class="card-body">
-      <h5 class="card-title ">Web Mobile Developer</h5>
-      <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-      <a href="#" class="btn btn-primary">Ver mas</a>
-    </div>
-    </div>
+      <div class="card text-center ">
+        <AiOutlineFileSearch />
+        <div class="card-body">
+          <h5 class="card-title ">Web Mobile Developer</h5>
+          <p class="card-text">
+            With supporting text below as a natural lead-in to additional
+            content.
+          </p>
+          <a href="#" class="btn btn-primary">
+            Ver mas
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
