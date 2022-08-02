741833358563934a30d17d985f29f8dfd538f18d
Matias Saravia <matiasaravia52@gmail.com>
2022-08-01 21:32:33 -0300

    se aplican estilos a las cards de las offers

--- src/components/Offer/index.js
+++ src/components/Offer/index.js
@@ -2,34 +2,34 @@ import React from "react";
 import "./style.css";
 import { AiOutlineFileSearch } from "react-icons/ai";
 import { GrLocation } from "react-icons/gr";
-import {BiBriefcase }from "react-icons/bi"; 
-import {BsCalendarDate, BsPeople} from "react-icons/bs";
+import { BiBriefcase } from "react-icons/bi";
+import { BsCalendarDate, BsPeople } from "react-icons/bs";
 const Offer = () => {
   return (
     <div className="card-offer">
-      <div class="card-header">     
-            <AiOutlineFileSearch class="search" type="button"/> 
-            <h5 > Web Mobile Developer</h5>
+      <div class="offer-header">
+        <AiOutlineFileSearch class="search" type="button" />
+        <h5> Web Mobile Developer</h5>
       </div>
-      <h7 ><GrLocation /> Buenos aires </h7>
-      <h7><BiBriefcase /> Jornada Completa</h7>
-      <br></br>
-      <h7><BsCalendarDate /> Hace un mes </h7>
-      <div class="card text-center ">
-          <div class="card-body">
-         
-          <p class="card-text">
-            With supporting text below as a natural lead-in to additional
-            content.
-          </p>
-          <a href="#" class="btn btn-primary">
-            Ver mas
-          </a>
-        </div>
-        
+      <div className="card-location">
+        <h7>
+          <GrLocation /> Buenos aires{" "}
+        </h7>
+        <h7>
+          <BiBriefcase /> Jornada Completa
+        </h7>
+        <h7>
+          <BsCalendarDate /> Hace un mes{" "}
+        </h7>
       </div>
+      <p class="offer-description">
+        With supporting text below as a natural lead-in to additional content.
+      </p>
+      <a href="#" class="btn">
+        Ver mas
+      </a>
       <div>
-          <BsPeople class="people" type="button" /> 
+        <BsPeople class="people" type="button" />
       </div>
     </div>
   );
