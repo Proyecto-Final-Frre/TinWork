2f052685b95b9d58f6a55eebef9505b674259cc5
Luciano <lucianonahuel4528@gmail.com>
2022-09-02 11:59:34 -0300

    se agrega tabla para mostrar información de candidatos

--- src/components/Candidates/index.js
+++ src/components/Candidates/index.js
@@ -13,7 +13,7 @@ const Candidates = () => {
       <Card.Title>Mobile developer</Card.Title>
     </div>
    </nav>
-   <div >
+   <div class="container-sidebar" >
    <ul class="nav nav-pills flex-column mb-auto">
       <li class="nav-item">
         <a href="#" class="nav-link link-dark" >
@@ -36,7 +36,43 @@ const Candidates = () => {
       
      
     </ul>
+    <div>
+    <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Nombre y apellido</th>
+      <th scope="col">Aptitudes coincidentes</th>
+      <th scope="col">Estado</th>
+      <th scope="col">Acciones</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>Matcheado</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>En espera</td>
+      <td>@twitter</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+      <td>Descartado</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
    </div>
+   </div>
+   
    </div>
    
    
