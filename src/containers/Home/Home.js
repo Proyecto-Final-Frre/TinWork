f4e9f8c280fd08ea02fc5b784a95b47fa3cabc52
Matias Saravia <matiasaravia52@gmail.com>
2022-09-24 20:29:35 -0300

    se eliminan botones del home

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,17 +1,7 @@
 import React from "react";
-import { authentication } from "../../config/firebase";
-import { logOut } from "../../config/firebase";
 
 const Home = () => {
-
-  return  <div>
-            <button onClick={authentication}>Sign In with Google</button >
-            <button onClick={logOut}> Log out </button>
-          </div> 
-
+  return <div></div>;
 };
 
 export default Home;
-
-
-
