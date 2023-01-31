38e77f24a25ac3cce33db9dfbee4b414e0b308c2
Matias Saravia <matiasaravia52@gmail.com>
2023-01-30 22:32:59 -0300

    se corrigen estilos

--- src/components/ProfileReclutier/index.js
+++ src/components/ProfileReclutier/index.js
@@ -1,16 +1,14 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 import { Card, Form, Button, Modal } from "react-bootstrap";
 import { storage } from "../../config/firebase";
 import { todasProvincias } from "../../services/ProvinceService";
-import SelectAvatar from 'react-avatar-edit'
+import SelectAvatar from "react-avatar-edit";
 import "./style.css";
 import { findUserByUid, updateProfile } from "../../services/UserService";
 import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import { useAuth } from "../../context/AuthContext";
 
-
 function ProfileReclutier() {
-
   const { user } = useAuth();
 
   //Manejo de botones
@@ -33,35 +31,32 @@ function ProfileReclutier() {
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
-  //Seleccion de sección de foto 
-  const [pview, setpview] = useState(false)
-
+  //Seleccion de sección de foto
+  const [pview, setpview] = useState(false);
 
   const onClose = () => {
-    setpview(null)
-  }
+    setpview(null);
+  };
 
   //Seteo de sección de la foto en pview
   const onCrop = (view) => {
-    setpview(view)
-  }
+    setpview(view);
+  };
 
   const saveCropImage = () => {
-
-    
     const imageRef = ref(storage, "image");
-    
+
     //Pasar la imagen seleccionada de base 64 a file
-    let base64string = pview
-    const [type, data] = base64string.split(',');
-    const mimeType = type.split(':')[1].split(';')[0];
+    let base64string = pview;
+    const [type, data] = base64string.split(",");
+    const mimeType = type.split(":")[1].split(";")[0];
     const text = atob(data);
     const buffer = new ArrayBuffer(text.length);
     const view = new Uint8Array(buffer);
     for (let i = 0; i < text.length; i++) {
       view[i] = text.charCodeAt(i);
     }
-    const file = new File([view], 'filename', { type: mimeType });
+    const file = new File([view], "filename", { type: mimeType });
     //=========================================================//
     uploadBytes(imageRef, file)
       .then(() => {
@@ -78,13 +73,12 @@ function ProfileReclutier() {
           .catch((error) => {
             console.log(error.message, "error al obtener la url de la imagen");
           });
-
       })
       .catch((error) => {
         console.log(error.message);
       });
-    setShow(false)
-  }
+    setShow(false);
+  };
   const editProfile = () => {
     const dataProfile = {
       description,
@@ -120,8 +114,9 @@ function ProfileReclutier() {
       let userDat = await findUserByUid(user.uid);
       setDescription(userDat?.description || "");
       setLocation(userDat?.location || "");
-      setUrl(userDat?.imageProfile ||
-        "https://w7.pngwing.com/pngs/223/244/png-transparent-computer-icons-avatar-user-profile-avatar-heroes-rectangle-black.png"
+      setUrl(
+        userDat?.imageProfile ||
+          "https://w7.pngwing.com/pngs/223/244/png-transparent-computer-icons-avatar-user-profile-avatar-heroes-rectangle-black.png"
       );
     };
     getDatByUidUser();
@@ -134,20 +129,20 @@ function ProfileReclutier() {
           <div className="grid">
             <div className="grid-1">
               <div className="image-container">
-                <img src={url} alt="logo" className="image" onClick={() => setShow(true)} />
-                <button
-                  type="button"
-                  className="boton"
-                  onClick={handleShow}
-                >
-                  Editar foto
-                </button>
+                <img
+                  src={url}
+                  alt="logo"
+                  className="image"
+                  onClick={() => setShow(true)}
+                />
               </div>
-
+              <button type="button" className="boton" onClick={handleShow}>
+                Editar foto
+              </button>
             </div>
             <div className="grid-2">
               <label htmlFor="name">
-                <font color="gray">Nombre/s y apellido/s</font>
+                <font color="gray">Nombre y apellido</font>
               </label>
               <p className="card-text">{user?.displayName || ""}</p>
               <label htmlFor="name">
@@ -157,23 +152,22 @@ function ProfileReclutier() {
             </div>
           </div>
           <div className="pie-foto">
-            <label htmlFor="name">
-              <font color="gray">Ubicación</font>{" "}
-              {!btnubi && (
-                <button
-                  type="button"
-                  className="button"
-                  onClick={() => setBtnubi(true)}
-                >
-                  Editar
-                </button>
-              )}
-            </label>
-            <p className="card-text">{location || ""}</p>
-
-            {btnubi && (
-              <div className="input-group mb-3">
-                <div>
+            <div style={{ padding: "5px" }}>
+              <label htmlFor="name">
+                <font color="gray">Ubicación</font>{" "}
+                {!btnubi && (
+                  <button
+                    type="button"
+                    className="button"
+                    onClick={() => setBtnubi(true)}
+                  >
+                    Editar
+                  </button>
+                )}
+              </label>
+              <p className="card-text">{location || ""}</p>
+              {btnubi && (
+                <div className="input-group mb-3">
                   <select
                     className="select-province"
                     aria-label=".form-select-sm example"
@@ -186,23 +180,22 @@ function ProfileReclutier() {
                       <option key={provincia.id}> {provincia.nombre}</option>
                     ))}
                   </select>
+                  <Button variant="primary" onClick={editProfile}>
+                    {" "}
+                    Guardar{" "}
+                  </Button>
+                  <Button variant="secondary" onClick={noeditubi}>
+                    {" "}
+                    Cancelar{" "}
+                  </Button>
                 </div>
-                <Button variant="primary" onClick={editProfile}>
-                  {" "} Guardar {" "}
-                </Button>
-                <Button variant="secondary" onClick={noeditubi}>
-                  {" "} Cancelar {" "}
-                </Button>
-              </div>
-            )}
+              )}
+            </div>
 
+            <div style={{ padding: "5px" }}>
+              <font color="gray">Descripción </font>
 
-            <font color="gray">Descripción </font>
-
-            {!btndescri && (
-
-              <div>
-                <p className="card-text">{description || ""}</p>
+              {!btndescri && (
                 <button
                   type="button"
                   className="button"
@@ -210,82 +203,82 @@ function ProfileReclutier() {
                 >
                   Editar
                 </button>
-             </div>
-
-            )}
-            {btndescri && (
-              <div className="card-body">
-                <Form.Group
-                  controlId="position-description"
-                >
-                  <Form.Control
-                    as="textarea"
-                    rows={6}
-                    placeholder="Descripción de la posición"
-                    value={description}
-                    onChange={(e) => setDescription(e.target.value)}
-                    type="text"
-
-                    onInput={(e) => {
-                      e.target.value.length > 60
-                        ? setButtonDisable(false)
-                        : setButtonDisable(true);
-                    }}
-                    required
-                  />
-                  <label
-                    style={{
-                      display: "flex",
-                      justifyContent: "flex-end",
-                      marginRight: 5,
-                    }}
-                  >
-                    {description.length - 60}
-
-                  </label>
-
-                </Form.Group>
-                <div className="botones">
-                  <Button variant="primary" onClick={editProfile} disabled={buttonDisable}>
-                    Guardar
-                  </Button>
+              )}
+              <p className="card-text">{description || ""}</p>
+              {btndescri && (
+                <div className="card-body">
+                  <Form.Group controlId="position-description">
+                    <Form.Control
+                      as="textarea"
+                      rows={6}
+                      placeholder="Descripción de la posición"
+                      value={description}
+                      onChange={(e) => setDescription(e.target.value)}
+                      type="text"
+                      onInput={(e) => {
+                        e.target.value.length > 60
+                          ? setButtonDisable(false)
+                          : setButtonDisable(true);
+                      }}
+                      required
+                    />
+                    <label
+                      style={{
+                        display: "flex",
+                        justifyContent: "flex-end",
+                        marginRight: 5,
+                      }}
+                    >
+                      {description.length - 60}
+                    </label>
+                  </Form.Group>
+                  <div className="botones">
+                    <Button
+                      variant="primary"
+                      onClick={editProfile}
+                      disabled={buttonDisable}
+                    >
+                      Guardar
+                    </Button>
+                  </div>
+                  <div className="botones">
+                    <Button variant="secondary" onClick={noeditdesc}>
+                      Cancelar
+                    </Button>
+                  </div>
                 </div>
-                <div className="botones">
-                  <Button variant="secondary" onClick={noeditdesc}>
-                    Cancelar
-                  </Button>
-                </div>
-              </div>
-            )}
+              )}
+            </div>
           </div>
         </Card.Body>
-        <div >
-
-        </div>
-        <Modal show={show} onHide={handleClose} style={{ marginTop: '10%' }} backdrop={1}>
-          <Modal.Dialog >
-
-            <Modal.Title >Seleccione foto de perfil</Modal.Title>
+        <div></div>
+        <Modal
+          show={show}
+          onHide={handleClose}
+          style={{ marginTop: "10%" }}
+          backdrop={1}
+        >
+          <Modal.Dialog>
+            <Modal.Title>Seleccione foto de perfil</Modal.Title>
             <SelectAvatar
               width={300}
               height={300}
               onCrop={onCrop}
               onClose={onClose}
-              
-
             />
             <Modal.Footer>
-              <Button variant="secondary" onClick={handleClose}>Cancelar</Button>
-              <Button variant="primary" onClick={saveCropImage}>Subir</Button>
+              <Button variant="secondary" onClick={handleClose}>
+                Cancelar
+              </Button>
+              <Button variant="primary" onClick={saveCropImage}>
+                Subir
+              </Button>
             </Modal.Footer>
           </Modal.Dialog>
         </Modal>
-
       </div>
     </div>
-
-
-  )
+  );
 }
 
-export default ProfileReclutier
+export default ProfileReclutier;
