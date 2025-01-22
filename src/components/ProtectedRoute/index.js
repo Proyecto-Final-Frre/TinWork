95dfd406ff755d75710b5dff456d0b8948301ad6
Luciano Lugo <lucianonahuel4528@gmail.com>
2025-01-22 17:45:09 -0300

    Implemento slash de carga al inicio de la app

--- src/components/ProtectedRoute/index.js
+++ src/components/ProtectedRoute/index.js
@@ -1,15 +1,25 @@
+import React, { Suspense } from 'react';
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../../context/AuthContext";
+import SplashScreen from '../Splash/SplashScreen';
 
- function ProtectedRoute({ children }) {
+function ProtectedRoute({ children }) {
   const { user, loading } = useAuth();
 
-  if (loading) return <h1>Loading</h1>;
+  if (loading) {
+    return (
+      <Suspense fallback={<div>Cargando...</div>}>
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+          <SplashScreen />
+          {/* <p>Cargando...</p> */}
+        </div>
+      </Suspense>
+    );
+  }
 
   if (!user) return <Navigate to="/login" />;
 
   return <>{children}</>;
 }
-
 
 export default ProtectedRoute;
