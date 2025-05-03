dcfed1007df30050a5fad3f2406722ce99b47ca7
Luciano Lugo <lucianonahuel4528@gmail.com>
2025-05-03 11:09:26 -0300

    Subo orden de provincias alfabeticamente

--- src/services/ProvinceService.js
+++ src/services/ProvinceService.js
@@ -4,7 +4,11 @@ const todasProvincias = async () => {
   const peticion = await axios.get(
     "https://apis.datos.gob.ar/georef/api/provincias"
   );
-  return peticion.data.provincias;
-};
 
+  const provinciasOrdenadas = peticion.data.provincias.sort((a, b) =>
+    a.nombre.localeCompare(b.nombre)
+  );
+
+  return provinciasOrdenadas;
+};
 export { todasProvincias };
