2fea5754cbd8a9d5085b021ef12545c1a73bbd05
Luciano Lugo <lucianonahuel4528@gmail.com>
2025-06-01 22:44:36 -0300

    Mejoras en province service

--- src/services/ProvinceService.js
+++ src/services/ProvinceService.js
@@ -5,9 +5,15 @@ const todasProvincias = async () => {
     "https://apis.datos.gob.ar/georef/api/provincias"
   );
 
-  const provinciasOrdenadas = peticion.data.provincias.sort((a, b) =>
-    a.nombre.localeCompare(b.nombre)
-  );
+  const provinciasOrdenadas = peticion.data.provincias
+  .filter((provincia) => provincia.nombre !== "Ciudad Autónoma de Buenos Aires")
+   .map((provincia) => {
+      if (provincia.nombre === "Tierra del Fuego, Antártida e Islas del Atlántico Sur") {
+        return { ...provincia, nombre: "Tierra del Fuego" };
+      }
+      return provincia;
+    })
+  .sort((a, b) => a.nombre.localeCompare(b.nombre));
 
   return provinciasOrdenadas;
 };
